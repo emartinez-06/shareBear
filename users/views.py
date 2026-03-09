a4c481622f733239cab2762430820cb4a0150f85
Erick Martinez <erickmartinez0131@gmail.com>
2026-03-09 12:52:32 -0500

    new view for frontend

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('/') # Redirect to home page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
