a8e18f784d8a852b817920269c6e8dcc853245fe
Erick Martinez <erickmartinez0131@gmail.com>
2026-03-21 18:57:15 -0500

    lgtm

--- waitlist/urls.py
+++ waitlist/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('process/', views.process, name='process'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
 ]
