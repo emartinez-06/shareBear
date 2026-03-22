2764ccbda40ef579cc69209f34ecb43acb2e8404
Erick Martinez <erickmartinez0131@gmail.com>
2026-03-22 14:36:17 -0500

    reverted changes

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-# Run collectstatic
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
