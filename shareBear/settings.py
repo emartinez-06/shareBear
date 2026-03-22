ae6d6470d3c9d02aa31eea419971928c26e06dd4
Erick Martinez <erickmartinez0131@gmail.com>
2026-03-22 14:00:22 -0500

    fixed static assets path | allowed whitenoise to collect assests/ to safely compress and hash images in deployment

--- shareBear/settings.py
+++ shareBear/settings.py
@@ -142,9 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'assets',
+]
 
 # Media files (Local storage)
 MEDIA_URL = '/media/'
@@ -155,6 +158,6 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
