32d858930e71645a71168560fb55a9ab412257af
Erick Martinez <erickmartinez0131@gmail.com>
2026-03-23 18:11:50 -0500

    build folder in gitignore

--- shareBear/settings.py
+++ shareBear/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'assets',
